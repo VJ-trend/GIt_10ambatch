f4c5e467a5a5ddfe55a71cf209288505d5b97486
VJ-trend <vijaytrendnologies@gmail.com>
2024-10-21 10:13:08 +0530

    First push

---
+++ src/main/java/com/maven_12pm_batch/App.java
@@ -0,0 +1,13 @@
+package com.maven_12pm_batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
